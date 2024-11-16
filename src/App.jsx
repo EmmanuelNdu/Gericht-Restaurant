94873aa27ea4cc58f385d502188d739348913644
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-11-17 00:14:11 +0100

    Started the navbar

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { React, useState } from 'react'
+import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu} from './container'
+import { Navbar} from './components'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <Navbar />
+     <Header />
+     <AboutUs />
+     <SpecialMenu />
+     <Chef />
+     <Intro />
+     <Laurels />
+     <Gallery />
+     <FindUs />
+     <Footer />
+    </>
+  )
+}
+
+export default App
