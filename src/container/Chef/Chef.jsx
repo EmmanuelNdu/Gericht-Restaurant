da1f379b871d06c610a56d81aedf76a37e27c67e
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-11-18 22:09:58 +0100

    completed the chef section

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
 
 import './Chef.css';
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className='app__bg app__wrapper section__padding'>
+    <div className='app__wrapper_img app__wrapper_img-reverse'>
+      <img  src={images.chef} alt='chef'/>
+    </div>
+
+    <div className='app__wrapper-info'>
+      <SubHeading title="Chef's word" />
+      <h1 className='headtext__cormorant'>What We Believe In</h1>
+
+      <div className='app__chef-content'>
+        <div className='app__chef-content_quote'>
+            <img src={images.quote} alt='quote' />
+            <p className='p__opensans'> During the eclipse, the lantern's velvet glow seemed to tangle.</p>
+        </div>
+        <p className='p__opensans'>During the eclipse, the lantern's velvet glow seemed to tangle with</p>
+      </div>
+
+      <div className='app__chef-sign'>
+        <p>Emmanuel Chukwuemeka</p>
+        <p className='p__opensans'>Chef & Founder</p>
+        <img src={images.sign} alt='sign' />
+      </div>
+    </div>
   </div>
 );
 
