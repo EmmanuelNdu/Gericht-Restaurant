9fab8ce52fcdccc39c3cc945b3dd24562e6ef2ff
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-11-20 14:00:29 +0100

    media query

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -4,10 +4,46 @@ import { SubHeading } from '../../components';
 import { images } from '../../constants';
 import './Gallery.css';
 
-const Gallery = () => (
-  <div>
-    Gallery
+const galleryImages = [ images.gallery01, images.gallery02, images.gallery03, images.gallery04]
+
+const Gallery = () => {
+   const scrollRef = React.useRef(null);
+
+   const scroll = (direction) => {
+    const { current } = scrollRef;
+
+    if (direction === 'left') {
+      current.scrollLeft -= 300;
+    } else {
+      current.scrollLeft += 300;
+    }
+   }
+
+  return (
+    <div className='app__gallery flex__center'>
+      <div className='app__gallery-content'>
+        <SubHeading title="instagram" />
+        <h1 className='headtext__cormorant'>Photo Gallery</h1>
+        <p className='p__opensans' style={{color: '#aaa', marginTop: '2rem'}}>During the eclipse, the lantern's velvet glow seemed to tangle with the mirage in the distance, while obsidian stones and ember</p>
+        <button type='button' className='custom__button'>View More</button>
+      </div>
+      <div className='app__gallery-images'>
+        <div className='app__gallery-images_container'>
+          { galleryImages.map((image, index) => (
+            <div className='app__gallery-images_card flex__center' key={`gallery_image-${index + 1}`}> 
+            <img src={image} alt='gallery' />
+            <BsInstagram className='gallery__image-icon' />
+            </div>
+          ))}
+        </div>
+        <div className='app__gallery-images_arrows'>
+          <BsArrowLeftShort className='gallery__arrow-icon' onClick={ () => scroll('left')} />
+          <BsArrowRightShort className='gallery__arrow-icon' onClick={ () => scroll('right')} />
+        </div>
+      </div>
   </div>
-);
+  )
+ 
+}
 
 export default Gallery;
