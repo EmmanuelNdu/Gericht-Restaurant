45ad5b28711c84afe81abfb5ebdd912e0ebb8bf4
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-11-21 16:08:58 +0100

    changes

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -8,7 +8,7 @@ const FindUs = () => (
       <SubHeading title="contact" />
       <h1 className='headtext__cormorant' style={{marginBottom: '3rem'}}>Find Us</h1>
       <div className='app__wrapper-content'>
-        <p className='p__opensans'>Lane End Bungalow,warcraft Lane Bartender T-pain</p>
+        <p className='p__opensans'>Lane End Bungalow,warcraft Lane Bartender Naruto</p>
         <p className='p__cormorant' style={{ color: '#dcca87', margin: '2rem 0'}}>Opening Hours</p>
         <p className='p__opensans'>Mon - Fri: 10:00 am - 2:00 am</p>
         <p className='p__opensans'>Sat - Sun: 10:00 am - 3:00 am</p>
